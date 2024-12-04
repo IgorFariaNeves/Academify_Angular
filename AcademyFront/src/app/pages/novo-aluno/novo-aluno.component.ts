292efda8bd5caf0fcf01f41bf3d93efbeb70d400
Igor <igorfaria31@gmail.com>
2024-12-04 16:15:41 -0300

    Projeto

--- AcademyFront/src/app/pages/novo-aluno/novo-aluno.component.ts
+++ AcademyFront/src/app/pages/novo-aluno/novo-aluno.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlunoService } from '../../service/Aluno.service';
 import { Router } from '@angular/router';
-import { firstValueFrom } from 'rxjs'; // Importando firstValueFrom
+import { firstValueFrom } from 'rxjs'; 
 
 @Component({
   selector: 'app-novo-aluno',
@@ -18,7 +18,7 @@ export class NovoAlunoComponent {
     private alunoService: AlunoService,
     private router: Router
   ) {
-    // Inicialização do formulário com validações
+    
     this.formAluno = this.formBuilder.group({
       nome: ['', [Validators.required, Validators.minLength(3)]],
       matricula: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
@@ -32,24 +32,24 @@ export class NovoAlunoComponent {
         const aluno = this.formAluno.value;
 
         if (aluno.nascimento instanceof Date) {
-          aluno.nascimento = aluno.nascimento.toISOString().split('T')[0]; // Formato 'YYYY-MM-DD'
+          aluno.nascimento = aluno.nascimento.toISOString().split('T')[0]; 
         }        
   
-        // Verifica se a data de nascimento é uma string ou Date e converte para o formato Date
+        
         if (typeof aluno.nascimento === 'string') {
-          aluno.nascimento = new Date(aluno.nascimento); // Converte para Date, se for string
+          aluno.nascimento = new Date(aluno.nascimento); 
         }
 
-        // Fazendo a requisição para cadastrar o aluno usando firstValueFrom
+        
         await firstValueFrom(this.alunoService.salvarAluno(aluno));
         alert('Aluno cadastrado com sucesso!');
-        this.router.navigate(['/alunos']); // Redireciona após o sucesso
+        this.router.navigate(['/alunos']); 
       } catch (erro: any) {
         this.mensagemErro =
           erro.error?.message || 'Erro ao cadastrar aluno. Tente novamente.';
       }
     } else {
-      // Caso o formulário seja inválido, exibe erros nos campos
+      
       this.mensagemErro = 'Por favor, preencha todos os campos corretamente.';
       Object.keys(this.formAluno.controls).forEach((campo) => {
         const controle = this.formAluno.get(campo);
